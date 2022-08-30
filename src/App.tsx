ece5ce7b49e97e770a601fefffe9bb831b9313fb
Eric Soros <ericsoros@Erics-MacBook-Pro.local>
2022-08-29 21:43:17 -0400

    adding login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
-import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  function login() {
+    alert("unable to login please try again")
+  }
+  
   return (
     <div className="App">
        <div style={{height: "40vh", width: "50vw", borderRadius: "1rem", backgroundColor: "white", color: "rgb(76,145,232)", background: "white", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center"}}>
         <span style={{fontSize: "5rem"}} className="material-symbols-outlined">install_desktop</span>
         <h3>Solace Software Inc</h3>
-        <button style={{width: "50%"}}>More Info</button>
+        <input placeholder='email' />
+        <input placeholder='password' />
+        <div style={{display: "flex", flexDirection: "row"}}>
+          <button onClick={login}>Login</button>
+          <button>Demo Code</button>
+        </div>
        </div>
     </div>
   )
